670a25a9819986095f0dd6b8e262c967faea52ec
Aghogho <monorienaghogho@gmail.com>
2023-07-06 23:53:04 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Lockboxes module"""
+
+
+def canUnlockAll(boxes):
+    keys = set([0])
+    while True:
+        prev_len = len(keys)
+        for i in range(len(boxes)):
+            if i in keys:
+                keys.update(boxes[i])
+        if len(keys) == prev_len:
+            break
+    return len(keys) == len(boxes)
